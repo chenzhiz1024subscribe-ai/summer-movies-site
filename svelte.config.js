8623c035e92e25b2bd1aad3f5075d23d48d5b570
chenzhiz1024subscribe-ai <chenzhiz1024subscribe@gmail.com>
2026-02-19 02:06:25 -0600

    Add GitHub Actions deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 const config = {
   kit: {
     adapter: adapter({
       pages: 'build',
-      assets: 'build'
+      assets: 'build',
+      fallback: 'index.html'
     }),
     paths: {
-      base: dev ? '' : '/A1-Summer-Movies'
+      base: '/summer-movies-site'
     }
   }
 };
